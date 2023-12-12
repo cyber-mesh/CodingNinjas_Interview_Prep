2104416bdee58058ca78d556e2c1eccfca6a27d3
cyber-mesh <147906204+cyber-mesh@users.noreply.github.com>
2023-12-12 23:55:56 +0530

    Create 32. Sorting the stack recursively.java

---
+++ 32. Sorting the stack recursively.java
@@ -0,0 +1,8 @@
+import java.util.Stack;
+import java.util.Collections;
+public class Solution {
+	public static Stack<Integer> sortStack(Stack<Integer> s) {
+		Collections.sort(s);
+		return s;
+	}
+}
