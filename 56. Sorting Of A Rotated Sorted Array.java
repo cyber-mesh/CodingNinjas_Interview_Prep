06fbe5d3d2fe575de09f8fb5a0eccd9ae3f521fb
cyber-mesh <147906204+cyber-mesh@users.noreply.github.com>
2023-12-15 22:54:35 +0530

    Create 56. Sorting Of A Rotated Sorted Array.java

---
+++ 56. Sorting Of A Rotated Sorted Array.java
@@ -0,0 +1,9 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.Arrays;
+public class Solution {
+    public static void sortRotatedArray(int[] arr, int n) {
+        Arrays.sort(arr);
+    }
+
+}
