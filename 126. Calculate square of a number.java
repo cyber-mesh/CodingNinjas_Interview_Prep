ac09760d1897abadfbbd9f6d10004580c45ab101
cyber-mesh <147906204+cyber-mesh@users.noreply.github.com>
2024-01-02 23:18:14 +0530

    Create 126. Calculate square of a number.java

---
+++ 126. Calculate square of a number.java
@@ -0,0 +1,11 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution 
+{
+    public static int calculateSquare(int num) 
+    {
+       return num*num;
+    }
+}
