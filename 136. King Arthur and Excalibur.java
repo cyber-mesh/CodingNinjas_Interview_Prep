e1726c3e9b5c9cb81bc903104b7fef2a1ee059b5
cyber-mesh <147906204+cyber-mesh@users.noreply.github.com>
2024-01-03 22:47:50 +0530

    Create 136. King Arthur and Excalibur.java

---
+++ 136. King Arthur and Excalibur.java
@@ -0,0 +1,14 @@
+public class Solution {
+
+
+	public static void main(String[] args) {
+		/* 
+			* Your class should be named Solution.
+			* Read input as specified in the question.
+			* Print output as specified in the question.
+			* Write your code here
+		 */
+
+	}
+
+}
