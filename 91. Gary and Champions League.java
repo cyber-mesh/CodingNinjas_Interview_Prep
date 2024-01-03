7554746e670535b84513ed557982f33f824ae9fe
cyber-mesh <147906204+cyber-mesh@users.noreply.github.com>
2024-01-03 23:39:02 +0530

    Update 91. Gary and Champions League.java

--- 91. Gary and Champions League.java
+++ 91. Gary and Champions League.java
@@ -1,1 +1,18 @@
-//
+import java.util.Arrays;
+public class Solution {
+
+	public static long maxProfit(int m, int n, int[] input) {
+		int len= m;
+		int sum=0;
+		Arrays.sort(input);
+		for(int i=n; i>0; i--)
+		{
+			if(input[len-1]<input[len-2])
+			{
+				len--;
+				sum+=arr[len-1];
+			}
+		}	
+	}
+	
+}
