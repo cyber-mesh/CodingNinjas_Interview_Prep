30a8e67bda144ea415c8f722dd42da46e76cbe18
cyber-mesh <147906204+cyber-mesh@users.noreply.github.com>
2024-01-04 00:07:06 +0530

    Create 85. Rat In a Maze All Paths.java

---
+++ 85. Rat In a Maze All Paths.java
@@ -0,0 +1,19 @@
+/*
+	Note:
+	To get all the test cases accepted, make recursive calls in following order: Top, Down, Left, Right.
+	This means that if the current cell is (x, y), then order of calls should be: top cell (x-1, y), 
+	down cell (x+1, y), left cell (x, y-1) and right cell (x, y+1).
+*/  
+
+public class Solution {
+
+
+	static void ratInAMaze(int maze[][], int n) {
+		/* 
+			* Your class should be named Solution.
+			* Write your code here
+		 */
+
+	}
+
+}
