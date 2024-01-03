d58219c19ec80d4e602ab77bb80a9d5e04c8ac7d
cyber-mesh <147906204+cyber-mesh@users.noreply.github.com>
2024-01-03 23:00:09 +0530

    Create 134. School Reunion.java

---
+++ 134. School Reunion.java
@@ -0,0 +1,11 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.List;
+
+public class Solution {
+    static List<Integer> schoolGroups(int n, int m, int q, 
+     int[][] relations, int[][] queries) {
+        // Write your code here.
+    }
+
+}
