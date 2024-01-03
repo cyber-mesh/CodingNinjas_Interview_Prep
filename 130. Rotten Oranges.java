b2a22d228137e5e8f9f69133aed1881faa8c5da8
cyber-mesh <147906204+cyber-mesh@users.noreply.github.com>
2024-01-03 23:05:23 +0530

    Create 130. Rotten Oranges.java

---
+++ 130. Rotten Oranges.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		/* Your class should be named Main.
+ 			* Read input as specified in the question.
+ 			* Print output as specified in the question.
+		*/
+
+		// Write your code here
+
+	}
+
+}
