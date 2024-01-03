6b8be47e702b38ef51cb06d80a236906ec023bb6
cyber-mesh <147906204+cyber-mesh@users.noreply.github.com>
2024-01-03 23:04:03 +0530

    Create 128. Domino.java

---
+++ 128. Domino.java
@@ -0,0 +1,12 @@
+
+import java.io.*;
+import java.util.*;
+public class Solution{
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        // Your class should be named Solution
+        // Write your code here
+
+	}
+}
